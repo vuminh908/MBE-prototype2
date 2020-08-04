191122d7472b227c027a89115c4e4c40e1aa1b0d
vuminh908 <minhv17@vt.edu>
2020-08-04 00:15:17 -0400

    Serial debug initial commit

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Modular Biolocomotion Emulator v2
 
 ## Teensy Source Files:
 * `mbe-teensy`: Primary code to control the MBE
+* `serial-rx-debug`: For debugging serial line communication (programmed onto Teensy 3.6)
 * `servo-test`: Code to test and set ID of single D956WP servo
 
-These have been written for and tested with **Teensy 3.2**
+These have been written for and tested with **Teensy 3.2** (unless otherwise noted)
---
+++ teensy/serial-debug/serial-debug.ino
@@ -0,0 +1,42 @@
+// Simple serial reader to debug serial communication
+// Prints anything received in serial port to USB serial port in readable form
+
+#define SERIAL Serial1  // Serial port to use for input
+
+const unsigned long baudRate = 115200;
+byte rb;
+char out[3]; // 2 to represent byte value in hex, 1 more for null terminator
+bool printCRLF = false;
+
+bool sameLine = false; // <-- Set to true to print bursts of bytes on same line
+
+void setup() {
+  Serial.begin(baudRate);
+  SERIAL.begin(baudRate);
+  
+  // Turn on built-in LED so we know the Teensy is on and the setup completed
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  // Display message
+  Serial.println("=== Serial RX ===");
+} // End setup function
+
+void loop() {
+  while (SERIAL.available() > 0)
+  {
+    rb = SERIAL.read();
+    sprintf(out, "%02X ", rb);
+    Serial.write(out);
+    if (sameLine)
+      printCRLF = true;
+    else
+      Serial.println();
+  }
+
+  if (printCRLF && sameLine)
+  {
+    Serial.println();
+    printCRLF = false;
+  }
+} // End loop function
